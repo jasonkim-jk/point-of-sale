3ba5353e86f5fea265d0b577670540af06cd56cc
julius oh <juliusoh@gmail.com>
2020-08-05 13:28:33 -0700

    user plan front end part 2

--- server/index.js
+++ server/index.js
@@ -13,15 +13,23 @@ app.use(sessionMiddleware);
 
 app.use(express.json());
 
-app.get('/api/health-check', (req, res, next) => {
-  db.query('select \'successfully connected\' as "message"')
-    .then(result => res.json(result.rows[0]))
-    .catch(err => next(err));
+app.get('/api/restaurant', (req, res, next) => {
+  const sql = `
+    select *
+    from "tables"
+  `;
+  db.query(sql)
+    .then(result => {
+      res.status(200).json(result.rows);
+    })
+    .catch(error => {
+      next(error);
+    });
 });
 
-// app.use('/api', (req, res, next) => {
-//   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
-// });
+app.use('/api', (req, res, next) => {
+  next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
+});
 
 app.use((err, req, res, next) => {
   if (err instanceof ClientError) {
@@ -32,21 +40,6 @@ app.use((err, req, res, next) => {
       error: 'an unexpected error occurred'
     });
   }
-});
-
-app.get('/api/restaurant', (req, res, next) => {
-  const sql = `
-    select *
-    from "tables"
-  `;
-
-  db.query(sql)
-    .then(result => {
-      res.status(200).json(result.rows);
-    })
-    .catch(error => {
-      next(error);
-    });
 });
 
 app.listen(process.env.PORT, () => {
