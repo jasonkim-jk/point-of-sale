25a894e5aa135bbc6a9dd2dbc115bb2963b52ceb
Kevin Lenell <krlenell@gmail.com>
2020-08-07 12:59:09 -0700

    fixed css for list

--- client/components/ViewChecks.jsx
+++ client/components/ViewChecks.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
+import Divider from '@material-ui/core/Divider';
 
 export default class ViewChecks extends React.Component {
   constructor(props) {
@@ -25,38 +26,41 @@ export default class ViewChecks extends React.Component {
     // 2020-06-03T07:00:00.000Z
     const time = crap.split('T')[1];
     const fullTime = time.split(':');
-    const hrs = fullTime[0];
+    let hrs = fullTime[0];
     const mins = fullTime[1];
+    const AMOrPM = parseInt(hrs) === 24 || parseInt(hrs) < 12 ? 'AM' : 'PM';
+    if (hrs > 12) {
+      hrs -= 12;
+    }
     const hrsAndMinutes = `${hrs}:${mins}`;
-    const AMOrPM = parseInt(hrs) < 12 ? 'AM' : 'PM';
     return `${hrsAndMinutes} ${AMOrPM}`;
   }
 
   render() {
 
     const checks = this.state.openChecks.map(check => (
-      <Grid item xs={12} className="item-container" key={check.checkId}>
+      <Grid item xs={12} key={check.checkId}>
         <Grid container spacing={1}>
-          <Grid item xs={6} className="left-check">
+          <Grid item xs={6}>
             <Typography component="div">
               <Box fontWeight="fontWeightBold">
-                Order {check.checkId} <br></br>
+                Check #{check.checkId}
               </Box>
-              {this.parseTime(check.createdAt)} <br></br>
+              {this.parseTime(check.createdAt)}
             </Typography>
           </Grid>
-          <Grid item xs={5} className="right-check">
-            <Typography>
-            Table {check.tableId} <br></br>
-
+          <Grid item xs={5}>
+            <Typography variant="h6" align="right">
+            Table {check.tableId}
             </Typography>
           </Grid>
         </Grid>
+        <Divider/>
       </Grid>
     ));
 
     return (
-      <Box width="50%" fontWeight="fontWeightBold">
+      <Box m={2} width="50%" fontWeight="fontWeightBold">
         <Grid container spacing={1}>
           {checks}
         </Grid>
