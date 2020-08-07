2222dba3b170113266bcec92ace5c7c18b361c3c
julius oh <juliusoh@gmail.com>
2020-08-07 11:22:37 -0700

    user view checks front end

--- client/components/ViewChecks.jsx
+++ client/components/ViewChecks.jsx
@@ -3,8 +3,10 @@ import Button from '@material-ui/core/Button';
 import {
   Link
 } from 'react-router-dom';
-import Box from '@material-ui/core/Box';
+
 import Typography from '@material-ui/core/Typography';
+
+import Grid from '@material-ui/core/Grid';
 
 export default class ViewChecks extends React.Component {
   constructor(props) {
@@ -25,36 +27,48 @@ export default class ViewChecks extends React.Component {
   }
 
   parseTime(crap) {
+    // 2020-06-03T07:00:00.000Z
     const time = crap.split('T')[1];
     const fullTime = time.split(':');
     const hrs = fullTime[0];
     const mins = fullTime[1];
     const hrsAndMinutes = `${hrs}:${mins}`;
-    const AMOrPM = parseInt(hrs) === 24 || parseInt(hrs) < 12 ? 'AM' : 'PM';
+    const AMOrPM = parseInt(hrs) < 12 ? 'AM' : 'PM';
     return `${hrsAndMinutes} ${AMOrPM}`;
   }
 
   render() {
     const { url } = this.props;
-    const checks = this.state.openChecks.map(check => (
-      <Typography component="div" key={check.checkId}>
-        Order {check.checkId} <br></br>
-        Time {this.parseTime(check.createdAt)} <br></br>
-        Table {check.tableId} <br></br>
-        <Button size="small" variant="contained" color="primary">
-          <Link to={`${url}/checkitem/${check.checkId}`}>
-            View Check
-          </Link>
-        </Button>
-        <hr className="new1"></hr>
-      </Typography>
 
+    const checks = this.state.openChecks.map(check => (
+      <Grid item xs={12} className="item-container" key={check.checkId}>
+        <Grid container spacing={1}>
+          <Grid item xs={6} className="left-check">
+            <Typography>
+            Order {check.checkId} <br></br>
+              {this.parseTime(check.createdAt)} <br></br>
+            </Typography>
+          </Grid>
+          <Grid item xs={5} className="right-check">
+            <Typography>
+            Table {check.tableId} <br></br>
+              <Button className="stupid-button" size="small" variant="contained" color="primary">
+                <Link to={`${url}/checkitem/${check.checkId}`}>
+              View Check
+                </Link>
+              </Button>
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
     ));
 
     return (
-      <Box className="checks-container">
+
+      <Grid container spacing ={1} fontWeight="fontWeightBold" className="checks-container" >
         {checks}
-      </Box>
+      </Grid>
+
     );
   }
 }
