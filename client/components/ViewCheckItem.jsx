12e5772931349380e236a845dc2f4cc72fa8837c
julius oh <juliusoh@gmail.com>
2020-08-07 15:32:51 -0700

    Waiter can create and view the current check for a table

---
+++ client/components/ViewCheckItem.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export default class ViewCheckItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openCheck: {}
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/checks')
+      .then(res => res.json())
+      .then(data => this.setState({ menus: data }))
+      .catch(() => console.error('server response error'));
+  }
+
+  render() {
+    const { params } = this.props.match;
+    return (
+      <Typography component="div">
+       Check {params.checkId}
+      </Typography>
+    );
+  }
+}
--- client/components/ViewRestaurant.jsx
+++ client/components/ViewRestaurant.jsx
@@ -3,17 +3,27 @@ import Box from '@material-ui/core/Box';
 
 import FloorPlan from './FloorPlan';
 import ViewChecks from './ViewChecks';
+import ViewCheckItem from './ViewCheckItem';
+import {
+  Switch,
+  Route
+} from 'react-router-dom';
 
 export default class ViewRestaurant extends React.Component {
 
   render() {
-
+    let { path, url } = this.props.match;
+    path = path.replace(/\/$/, '');
+    url = url.replace(/\/$/, '');
     return (
 
       <Box display="flex">
 
-        <ViewChecks />
-        <FloorPlan />
+        <ViewChecks url={url} />
+        <Switch>
+          <Route path={`${path}/checkitem/:checkId`} component={ViewCheckItem} />
+          <Route exact path={path} component={FloorPlan} />
+        </Switch>
 
       </Box>
     );
