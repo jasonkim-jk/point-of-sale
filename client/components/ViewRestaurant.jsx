28ec5b9242398459045347e371bd0087b564cc30
julius oh <juliusoh@gmail.com>
2020-08-07 12:07:35 -0700

    reverted to single feature

--- client/components/ViewRestaurant.jsx
+++ client/components/ViewRestaurant.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import FloorPlan from './FloorPlan';
 import ViewChecks from './ViewChecks';
-import ViewCheckItem from './ViewCheckItem';
-import {
-  Switch,
-  Route
-} from 'react-router-dom';
 
 export default class ViewRestaurant extends React.Component {
 
   render() {
-    let { path, url } = this.props.match;
-    path = path.replace(/\/$/, '');
-    url = url.replace(/\/$/, '');
+
     return (
 
       <div className='view-restaurant-container'>
-        <ViewChecks url={url} />
-        <Switch>
-          <Route path={`${path}/checkitem/:checkId`} component={ViewCheckItem} />
-          <Route exact path={path} component={FloorPlan} />
-        </Switch>
+        <ViewChecks />
+
+        <FloorPlan />
+
       </div>
     );
   }
