0764cd06c6d112dca1b4c43e337667dc0227c933
Kevin Lenell <krlenell@gmail.com>
2020-08-09 20:41:56 -0700

    re-added delete endpoint, working on delete front end

--- client/components/WaitListTable.jsx
+++ client/components/WaitListTable.jsx
@@ -8,7 +8,8 @@ export default class WaitListTable extends React.Component {
   render() {
     const waitList = this.props.waitList;
     const waitListItems = waitList.map(waiting => {
-      return <WaitListTableItem seatCustomer={this.props.seatCustomer} updateList={this.props.updateList} key={waiting.waitId} root={waiting}/>;
+      return <WaitListTableItem deleteCustomer={this.props.deleteCustomer} seatCustomer={this.props.seatCustomer}
+        updateList={this.props.updateList} key={waiting.waitId} root={waiting}/>;
     });
     return (
       <TableContainer component={Paper}>
@@ -20,6 +21,7 @@ export default class WaitListTable extends React.Component {
               <TableCell>Name</TableCell>
               <TableCell>Wait Time</TableCell>
               <TableCell>Comment</TableCell>
+              <TableCell padding="checkbox"></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
