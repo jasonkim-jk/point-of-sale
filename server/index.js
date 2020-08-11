e683081bc78b42151f77553c189d51a3bd97d69f
Jason Kim <thehesedlife@gmail.com>
2020-08-11 00:26:56 -0700

    re-arranged endpoint function and added a new endpoint for patch /api/orderItems/:orderItemId

--- server/index.js
+++ server/index.js
@@ -246,16 +246,76 @@ app.get('/api/orders', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.post('/api/orders/', (req, res, next) => {
+  if (!checkValidity(req.body.tableId) || req.body.items.length === 0) {
+    return res.status(400).json({ error: 'Sorry, your order information is incomplete.' });
+  }
+
+  const paramDb = [parseInt(req.body.tableId), 'NOW()'];
+  const sql = `
+      insert into "orders" ("tableId", "orderedAt")
+          values ($1, $2)
+        returning *
+    `;
+
+  db.query(sql, paramDb)
+    .then(result => {
+      const orderId = result.rows[0].orderId;
+      const paramDb = getDbParam(orderId, req.body.items);
+      const sql = `
+          insert into "orderItems" ("orderId", "itemId", "quantity", "discount", "createdAt")
+        select * from UNNEST ($1::int[], $2::int[], $3::int[], $4::int[], $5::timestamp[])
+            returning "orderItemId"
+      `;
+
+      return db.query(sql, paramDb).then(result2 => {
+        const orderItemIds = result2.rows;
+        return { orderId, orderItemIds };
+      });
+    })
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(err => next(err));
+});
+
+app.patch('/api/orderItems/:orderItemId', (req, res, next) => {
+  if (!checkValidity(req.params.orderItemId)) {
+    return res.status(400).json({ error: 'Sorry, your requested id is invalid.' });
+  }
+
+  const orderItemId = parseInt(req.params.orderItemId);
+  const isCompleted = req.body.isCompleted;
+  const paramDb = [orderItemId, isCompleted];
+  const sql = `
+      update "orderItems"
+         set "isCompleted" = $2
+       where "orderItemId" = $1
+   returning *
+  `;
+
+  db.query(sql, paramDb)
+    .then(result => {
+      if (result.rows[0] === undefined) {
+        return res.status(404).json({ error: 'Requested id may not exist in the database. Check your data agin.' });
+      } else {
+        res.status(200).json(result.rows[0]);
+      }
+    })
+    .catch(err => next(err));
+
+});
+
 app.get('/api/sales', (req, res, next) => {
   const sql = `
-  select "menus"."item" as "Item Name",
-         "menus"."imageUrl" as "Image",
-         sum("orderItems"."quantity") as "Total Sold",
-         "menus"."salePrice" as "Sale Price",
-         "menus"."cost" as "Cost",
-         ("menus"."salePrice" - "menus"."cost") * sum("orderItems"."quantity") as "Profit"
-    from "menus"
-    join "orderItems" using ("itemId")
+      select "menus"."item" as "Item Name",
+            "menus"."imageUrl" as "Image",
+            sum("orderItems"."quantity") as "Total Sold",
+            "menus"."salePrice" as "Sale Price",
+            "menus"."cost" as "Cost",
+            ("menus"."salePrice" - "menus"."cost") * sum("orderItems"."quantity") as "Profit"
+        from "menus"
+        join "orderItems" using ("itemId")
     group by ("menus"."item", "menus"."imageUrl", "menus"."salePrice", "menus"."cost")
     order by "Profit" desc;
     `;
@@ -279,50 +339,6 @@ app.get('/api/checks', (req, res, next) => {
     .catch(error => {
       next(error);
     });
-});
-
-app.post('/api/orders/', (req, res, next) => {
-  if (!checkValidity(req.body.tableId) || req.body.items.length === 0) {
-    return res.status(400).json({ error: 'Sorry, your order information is incomplete.' });
-  }
-
-  const paramDb = [parseInt(req.body.tableId), 'NOW()'];
-  const sql = `
-      insert into "orders" ("tableId", "orderedAt")
-          values ($1, $2)
-        returning *
-    `;
-
-  db.query(sql, paramDb)
-    .then(result => {
-      const orderId = result.rows[0].orderId;
-      const paramDb = getDbParam(orderId, req.body.items);
-      const sql = `
-          insert into "orderItems" ("orderId", "itemId", "quantity", "discount", "createdAt")
-        select * from UNNEST ($1::int[], $2::int[], $3::int[], $4::int[], $5::timestamp[])
-            returning "orderItemId"
-      `;
-
-      return db.query(sql, paramDb)
-        .then(result2 => {
-          const orderItemIds = result2.rows;
-          return { orderId, orderItemIds };
-        });
-    })
-    .then(result => {
-      res.status(201).json(result);
-    })
-    .catch(err => next(err));
-});
-
-app.get('/api/waitlist', (req, res, next) => {
-  const sql = `select * from "waitLists"
-  order by  "isSeated" asc, "time" asc;`;
-  db.query(sql)
-    .then(result => {
-      res.status(200).json(result.rows);
-    })
-    .catch(err => next(err));
 });
 
 app.get('/api/checks/:checkId', (req, res, next) => {
@@ -358,6 +374,16 @@ app.put('/api/checks/:checkId', (req, res, next) => {
       res.status(200).json(result.rows);
     })
     .catch(error => next(error));
+});
+
+app.get('/api/waitlist', (req, res, next) => {
+  const sql = `select * from "waitLists"
+  order by  "isSeated" asc, "time" asc;`;
+  db.query(sql)
+    .then(result => {
+      res.status(200).json(result.rows);
+    })
+    .catch(err => next(err));
 });
 
 app.post('/api/waitlist', (req, res, next) => {
